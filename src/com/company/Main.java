76ab65861f92fe32631ada6e8305c54a65aae202
TimofeyShved <timofeya123@inbox.ru>
2024-01-17 22:46:53 +0600

    char

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // Регулярные выражения (фильтр)
+
+        // Находим символ
+        Pattern p = Pattern.compile("a");
+        Matcher m = p.matcher("Jack is a boy");
+        while (m.find()){
+            System.out.println(m.start() + " " + m.group() +" ");
+        }
+
     }
 }
